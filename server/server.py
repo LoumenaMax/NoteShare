ad15bca1837d742e6dfd4c2ae960f6f5f4a81afc
Max Loumena <loumenamax@gmail.com>
2019-05-06 02:08:58 -0700

    Added endpoints for new procedures

--- server/server.py
+++ server/server.py
@@ -14,6 +14,52 @@ app.config['MYSQL_DATABASE_DB'] = 'NoteShare'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
+# Takes in the request and a list of variable names you want from the request and
+# outputs a tuple that either contains the varibles from the request or an error
+# that shows that the variables were not in the request
+#
+# Ex: Input: getRequestVariables(request, ["username"])
+# Ex: Good Output: (False, "xXExampleUsernameXx")
+# Ex: Bad Output: (True, "Enter the required fields")
+def getRequestVariables( request, varNames, nullFlag ):
+    try:
+        post_data = request.get_json()
+        instantiatedNames = []
+        for name in varNames:
+            real_data = post_data.get(name)
+            if real_data or nullFlag:
+                instantiatedNames.append(real_data)
+            else:
+                return True, "Enter the required fields"
+    except Exception as e:
+        return True, e
+    return False, instantiatedNames
+
+# Takes in a procedure name, a flag for if you will fetch one or fetch all from
+# the procedure, and a list of varibles to pass into the procedure, and returns
+# the result
+def getData( procName, oneFlag, instantiatedNames ):
+    conn = None
+    cursor = None
+    data = None
+    errorFlag = False
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        cursor.callproc(procName,tuple(instantiatedNames) if instantiatedNames else tuple([]))
+        if oneFlag:
+            data = cursor.fetchone()
+        else:
+            data = cursor.fetchall()
+    except Exception as e:
+        errorFlag = True
+        data = e
+    finally:
+        conn.commit() if cursor else None;
+        cursor.close() if cursor else None;
+        conn.close() if conn else None;
+    return (errorFlag, data)
+
 @app.route('/example', methods = ['POST'])
 def test():
     post_data = request.get_json()
@@ -22,101 +68,147 @@ def test():
             'message': post_data.get('ex')+"ABC"
         }))
 
-@app.route('/register',methods=['POST','GET'])
+@app.route('/register',methods=['POST'])
 def signUp():
-    try:
-        post_data = request.get_json()
-        _name = post_data.get("name")
-        _email = post_data.get("email")
-        _password = post_data.get("password")
+    instantiatedNames = getRequestVariables(request, ["email", "password"], False)
+    if instantiatedNames[0]:
+        return make_response(jsonify({'error': str(instantiatedNames[1])}))
 
-        # validate the received values
-        if _name and _email and _password:
+    username_password = instantiatedNames[1]
+    username_password[1] = generate_password_hash(username_password[1])
 
-            # All Good, let's call MySQL
+    data = getData('sp_createUser', False, username_password)
+    if data[0]:
+        return make_response(jsonify({'error': str(data[1])}))
+    data = data[1]
 
-            conn = mysql.connect()
-            cursor = conn.cursor()
-            _hashed_password = generate_password_hash(_password)
-            print(_hashed_password)
-            cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
-            data = cursor.fetchall()
-
-            if len(data) is 0:
-                conn.commit()
-                return make_response(jsonify(
-                    {
-                        'message': 'User created successfully'
-                    }))
-            else:
-                return make_response(jsonify(
-                    {
-                        'error': str(data[0])
-                    }))
-        else:
-            return make_response(jsonify(
-                {
-                    'message': 'Enter the required fields'
-                }))
-
-    except Exception as e:
+    if len(data) is 0:
         return make_response(jsonify(
             {
-                'error': str(e)
+                'message': 'User created successfully'
             }))
-    finally:
-        cursor.close()
-        conn.close()
+    else:
+        return make_response(jsonify(
+            {
+                'error': data[0][0]
+            }))
 
-@app.route('/login',methods=['POST','GET'])
+@app.route('/login',methods=['POST'])
 def login():
-    try:
-        post_data = request.get_json()
-        _email = post_data.get("email")
-        _password = post_data.get("password")
-
-        # validate the received values
-        if _email and _password:
-
-            # All Good, let's call MySQL
-
-            conn = mysql.connect()
-            cursor = conn.cursor()
-            sql = "SELECT * FROM users WHERE user_username = %s"
-
-            cursor.execute(sql, _email)
-            user = cursor.fetchone()
-
-            if user is None:
-                return make_response(jsonify(
-                    {
-                        'error': 'No user found'
-                    }))
-            elif check_password_hash(user[3], _password):
-                return make_response(jsonify(
-                    {
-                        'message': 'Success!'
-                    }))
-            else:
-                return make_response(jsonify(
-                    {
-                        'message': 'Incorrect Password'
-                    }))
-        else:
-            return make_response(jsonify(
-                {
-                    'message': 'Enter the required fields'
-                }))
-
-    except Exception as e:
+    instantiatedNames = getRequestVariables(request, ["email", "password"], False)
+    if instantiatedNames[0]:
         return make_response(jsonify(
             {
-                'error': str(e)
+                'error': str(instantiatedNames[1])
             }))
-    finally:
-        cursor.close()
-        conn.close()
+    username_password = instantiatedNames[1]
+    data = getData('sp_getLoginInfo', True, username_password[0:1])
+    if data[0]:
+        return make_response(jsonify({'error': str(data[1])}))
+    data = data[1]
 
+    if data[1] is None:
+        return make_response(jsonify(
+            {
+                'error': 'No user found'
+            }))
+    elif check_password_hash(data[2], username_password[1]):
+        return make_response(jsonify({'user': data[1]}))
+    else:
+        return make_response(jsonify({'error': 'Incorrect Password'}))
+
+@app.route('/searchClasses',methods=['POST'])
+def searchClasses():
+    instantiatedNames = getRequestVariables(request, ["school_id"], False)
+    if instantiatedNames[0]:
+        return make_response(jsonify(
+            {
+                'error': str(instantiatedNames[1])
+            }))
+    _school_id = instantiatedNames[1]
+    data = getData('sp_getClasses', False, _school_id)
+    if data[0]:
+        return make_response(jsonify({'error': str(data[1])}))
+    data = data[1]
+
+    if data[0] == "School does not exist":
+        return make_response(jsonify(
+            {
+                'error': data[0]
+            }))
+    else:
+        return make_response(jsonify(
+            {
+                'classes': data
+            }))
+
+@app.route('/searchSchools',methods=['GET'])
+def searchSchools():
+    data = getData('sp_getSchools', False, [])
+    if data[0]:
+        return make_response(jsonify({'error': str(data[1])}))
+    data = data[1]
+
+    if len(data) is not 0:
+        return make_response(jsonify(
+            {
+                'schools': data
+            }))
+    else:
+        return make_response(jsonify(
+            {
+                'schools': []
+            }))
+
+@app.route('/createUserInfo',methods=['POST'])
+def createUserInfo():
+    instantiatedNames = getRequestVariables(request, ["user_id", "display_name", "real_name", "email", "phone_number"], True)
+    if instantiatedNames[0]:
+        return make_response(jsonify(
+            {
+                'error': str(instantiatedNames[1])
+            }))
+    user_info = instantiatedNames[1]
+    data = getData('sp_createUserInfo', False, user_info)
+    if data[0]:
+        return make_response(jsonify({'error': str(data[1])}))
+    data = data[1]
+
+    if len(data) is 0:
+        return make_response(jsonify(
+            {
+                'message': 'UserInfo created successfully'
+            }))
+    else:
+        return make_response(jsonify(
+            {
+                'error': data[0][0]
+            }))
+
+@app.route('/createSchool',methods=['POST'])
+def createSchool():
+    instantiatedNames = getRequestVariables(request, ["school_name"], False)
+    if instantiatedNames[0]:
+        return make_response(jsonify(
+            {
+                'error': str(instantiatedNames[1])
+            }))
+    user_info = instantiatedNames[1]
+    data = getData('sp_createSchool', False, user_info)
+    if data[0]:
+        return make_response(jsonify({'error': str(data[1])}))
+    data = data[1]
+
+    if len(data) is 0:
+        return make_response(jsonify(
+            {
+                'message': 'School created successfully'
+            }))
+    else:
+        return make_response(jsonify(
+            {
+                'error': data[0][0]
+            }))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.environ.get('PORT', 8000), debug=True)
